b13bdac0eac1169701ee7c40c26afd8ff903deea
mdabarik <mdabarik19@gmail.com>
2023-12-06 02:52:21 +0600

    first commit

---
+++ src/app/projects/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectPage = () => {
+    return (
+        <div>
+            <h1>This is my project page!</h1>
+        </div>
+    );
+};
+
+export default ProjectPage;
